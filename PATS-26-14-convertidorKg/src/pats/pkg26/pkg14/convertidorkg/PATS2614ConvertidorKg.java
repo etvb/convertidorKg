a57688621d683a754b9d67f593b9711438b268b5
Edgardo Torres Vanden Bouch <edgardo.torres@hotmail.com>
2017-09-28 07:36:05 -0500

    archivos base

---
+++ PATS-26-14-convertidorKg/src/pats/pkg26/pkg14/convertidorkg/PATS2614ConvertidorKg.java
@@ -0,0 +1,20 @@
+/*
+Ejercicio 14: HAcer un programa que pase de Kg a otra unidad de medida de masa,
+mostrar en pantalla un menú con las opcioness posibles.
+ */
+package pats.pkg26.pkg14.convertidorkg;
+
+/**
+ *
+ * @author edgardo
+ */
+public class PATS2614ConvertidorKg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
